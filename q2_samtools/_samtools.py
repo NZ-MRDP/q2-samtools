0dd8b0526dff509a9cc23bb96f95ce08bb706fc8
jkliu <joli@novozymes.com>
2023-08-23 14:38:45 -0700

    sort working

--- q2_samtools/_samtools.py
+++ q2_samtools/_samtools.py
@@ -1,17 +1,28 @@
 import os
 import subprocess
-import tempfile
 
-import pandas as pd
-from q2_types.sample_data import SampleData
-from q2_types_genomics.per_sample_data._format import BAMDirFmt
-from q2_types_genomics.per_sample_data._type import AlignmentMap
+from q2_types_genomics.per_sample_data._format import BAMDirFmt, BAMFormat
 
 
+# TODO: Add in arguments/flags
+# TODO: Make sure .sam/.cram files work - low priority
+# TODO: maybe add another method that allows transformations from .sam/.cram to .bam
 def sort(alignment_map: BAMDirFmt) -> BAMDirFmt:
     """sort."""
     output_bam = BAMDirFmt()
-    for b in alignment_map:
-        cmd = ["samtools", "sort", str(alignment_map), "-o", str(output_bam)]
+    for path, _ in alignment_map.bams.iter_views(view_type=BAMFormat):  # type: ignore
+        cmd = [
+            "samtools",
+            "sort",
+            os.path.join(str(alignment_map.path), str(path.stem) + ".bam"),
+            "-o",
+            os.path.join(str(output_bam), str(path.stem) + ".bam"),
+        ]
         subprocess.run(cmd, check=True)
     return output_bam
+
+
+# TODO: Get this plugin working
+def faidx():
+    """faidx."""
+    pass
--- q2_samtools/_transformer.py
+++ q2_samtools/_transformer.py
@@ -2,13 +2,14 @@ from typing import List
 
 from q2_types_genomics.per_sample_data._format import BAMDirFmt, BAMFormat
 
+# to SAM -> TODO: copy code from q2_phylogenomics
+# to CRAM -> TODO: Write these transformers
+# @plugin.register_transformer
+# def _1(data: BAMDirFmt) -> List[BAMFormat]:
+#     list_bam_fmt = list()
+#     for output in data:
+#         filename = f"{output.sample_id}_{output.type}_keystones.json"
+#         with open(dirfmt.path / filename, "w") as fh:
+#             json.dump(output.dict(), fh)
 
-@plugin.register_transformer
-def _1(data: BAMDirFmt) -> List[BAMFormat]:
-    list_bam_fmt = list()
-    for output in data:
-        filename = f"{output.sample_id}_{output.type}_keystones.json"
-        with open(dirfmt.path / filename, "w") as fh:
-            json.dump(output.dict(), fh)
-
-    return dirfmt
+#     return dirfmt
