66cefe94e946fc1d7bc804464008914f55cd223a
mchenaux <99347548+mchenaux@users.noreply.github.com>
2023-08-29 11:41:48 -0700

    Samtools sort flag 1 (#3)
    * threads working
    
    * added sort flags
    
    * minor edits for pr
    
    * tag -> tag_sort
    
    * added input and output descriptions
    
    * fixed input output description formats
    
    * fixed input output description names
    
    * tag -> tag_sort
    
    ---------
    
    Co-authored-by: Megan Chenaux <mled@Megans-MacBook-Pro.local>
    Co-authored-by: Megan Chenaux <mled@LC215635.nzww.nzcorp.net>

--- q2_samtools/_samtools.py
+++ q2_samtools/_samtools.py
@@ -7,7 +7,17 @@ from q2_types_genomics.per_sample_data._format import BAMDirFmt, BAMFormat
 # TODO: Add in arguments/flags
 # TODO: Make sure .sam/.cram files work - low priority
 # TODO: maybe add another method that allows transformations from .sam/.cram to .bam
-def sort(alignment_map: BAMDirFmt) -> BAMDirFmt:
+#
+def sort(
+    alignment_map: BAMDirFmt,
+    threads: int = 1,
+    compression_level: int = 1,
+    memory_per_thread: str = "768M",
+    name_sort: bool = False,
+    tag_sort: str = "",
+    minimizer_sort: bool = False,
+    kmer_size: int = 20,
+) -> BAMDirFmt:
     """sort."""
     output_bam = BAMDirFmt()
     for path, _ in alignment_map.bams.iter_views(view_type=BAMFormat):  # type: ignore
@@ -15,9 +25,21 @@ def sort(alignment_map: BAMDirFmt) -> BAMDirFmt:
             "samtools",
             "sort",
             os.path.join(str(alignment_map.path), str(path.stem) + ".bam"),
+            "-@",
+            str(threads),
+            "-l",
+            str(compression_level),
+            "-m",
+            str(memory_per_thread),
             "-o",
             os.path.join(str(output_bam), str(path.stem) + ".bam"),
         ]
+        if name_sort:
+            cmd.append("-n")
+        if tag_sort:
+            cmd.extend(["-t", tag_sort])
+        if minimizer_sort:
+            cmd.extend(["-M", "-K", str(kmer_size)])
         subprocess.run(cmd, check=True)
     return output_bam
 
