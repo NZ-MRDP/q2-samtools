82740d3569cfe24038f859497b0bb390234e2ab0
johnchase <johnhchase@protonmail.com>
2024-02-23 13:49:47 -0800

    updates version

--- q2_samtools/__init__.py
+++ q2_samtools/__init__.py
@@ -1,6 +1,6 @@
 """samtools python library."""
 from ._samtools import extract_fasta_subsequence, index_sequences, sort
 
-__version__ = "0.1.4"
+__version__ = "0.1.7"
 
 __all__ = ["sort", "index_sequences", "extract_fasta_subsequence"]
